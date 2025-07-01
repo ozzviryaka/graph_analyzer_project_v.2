e13428c22c5a9bf76a897e9d7ef0ecd9dcf3f1a3
ozzviryaka <ozzviryaka@gmail.com>
2025-07-01 17:54:30 +0300

    Add localization support for traversal control widget in Ukrainian

--- gui/widgets/traversal_tab_widgets/traversal_control_widget.py
+++ gui/widgets/traversal_tab_widgets/traversal_control_widget.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QComboBox, QLabel
 from gui.additionals.readonly_graph_canvas import ReadOnlyGraphCanvas
+from locales.locale_manager import LocaleManager
 
 class TraversalControlWidget(QWidget):
     """
@@ -11,16 +12,16 @@ class TraversalControlWidget(QWidget):
         self.canvas = ReadOnlyGraphCanvas(graph, self)
         self.method_combo = QComboBox(self)
         self.method_combo.addItems([
-            'BFS (у ширину)',
-            'DFS (у глибину)',
-            'Dijkstra',
-            'Компоненти звʼязності',
-            'Пошук циклів'
+            LocaleManager.get_locale("traversal_control_widget", "bfs_method"),
+            LocaleManager.get_locale("traversal_control_widget", "dfs_method"),
+            LocaleManager.get_locale("traversal_control_widget", "dijkstra_method"),
+            LocaleManager.get_locale("traversal_control_widget", "connected_components_method"),
+            LocaleManager.get_locale("traversal_control_widget", "cycle_detection_method")
         ])
         self.start_vertex_combo = QComboBox(self)
         self.update_vertex_list()
-        self.start_btn = QPushButton('Запустити', self)
-        self.stop_btn = QPushButton('Стоп', self)
+        self.start_btn = QPushButton(LocaleManager.get_locale("traversal_control_widget", "start_button"), self)
+        self.stop_btn = QPushButton(LocaleManager.get_locale("traversal_control_widget", "stop_button"), self)
         self.status_label = None  # Видалено статус-лейбл
         self._setup_layout()
         # Тут можна додати підключення сигналів до методів запуску/зупинки
@@ -50,9 +51,9 @@ class TraversalControlWidget(QWidget):
     def _setup_layout(self):
         vbox = QVBoxLayout(self)
         hbox = QHBoxLayout()
-        hbox.addWidget(QLabel('Метод:', self))
+        hbox.addWidget(QLabel(LocaleManager.get_locale("traversal_control_widget", "method_label"), self))
         hbox.addWidget(self.method_combo)
-        hbox.addWidget(QLabel('Початкова вершина:', self))
+        hbox.addWidget(QLabel(LocaleManager.get_locale("traversal_control_widget", "start_vertex_label"), self))
         hbox.addWidget(self.start_vertex_combo)
         hbox.addWidget(self.start_btn)
         hbox.addWidget(self.stop_btn)
